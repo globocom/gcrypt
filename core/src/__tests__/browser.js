e3a28662b7e23ceaa658cee0865f41191b2df4a9
Claudio Netto <nettinhorama@gmail.com>
2018-12-11 15:51:18 -0200

    core: create login method

--- core/src/__tests__/browser.js
+++ core/src/__tests__/browser.js
@@ -1,4 +1,4 @@
-import BrowserClient from '../browser';
+import { BrowserClient, BrowserSessionStorage } from '../browser';
 
 describe('BrowserClient', () => {
   let client = null;
@@ -6,6 +6,7 @@ describe('BrowserClient', () => {
   beforeEach(() => {
     fetch.resetMocks();
     client = new BrowserClient('https://api.gcrypt.local:8443', { fetch });
+    client.sessionStorage.clear();
   });
 
   describe('constructor', () => {
@@ -20,6 +21,12 @@ describe('BrowserClient', () => {
     it('When setting is not defined, should use default settings', () => {
       const newClient = new BrowserClient('https://api.gcrypt.local:8443');
       expect(newClient.fetch).toBe(window.fetch);
+    });
+
+    it('When using a custom session storage, should use that instead default', () => {
+      const sessionStorage = new BrowserSessionStorage('another-session-key');
+      const newClient = new BrowserClient('https://api.gcrypt.local:8443', { sessionStorage });
+      expect(newClient.sessionStorage).toEqual(sessionStorage);
     });
   });
 
@@ -39,6 +46,165 @@ describe('BrowserClient', () => {
     it('When server is not working, should resolve to false', () => {
       fetch.once('DOWN', { status: 500 });
       return expect(client.health()).resolves.toEqual(false);
+    });
+  });
+
+  describe('login', () => {
+    it('When credential is not defined, should throw error', () => {
+      expect(() => client.login()).toThrowError('credential is required');
+    });
+
+    it('When credential is null, should throw error', () => {
+      expect(() => client.login(null)).toThrowError('credential is required');
+    });
+
+    describe('NativeCredential', () => {
+      it('When the token field is not defined, should omit it on body request', () => {
+        fetch.once(JSON.stringify({ token: '...' }));
+
+        const credential = {
+          username: 'gcrypt@gcrypt.globo.com',
+          password: 'awesome-security-testing',
+        };
+
+        return client.login(credential).then(() => {
+          expect(fetch).toHaveBeenCalled();
+          const request = fetch.mock.calls[0][0];
+          expect(request.method).toStrictEqual('POST');
+          expect(request.headers.get('Content-Type')).toContain('application/json');
+          expect(new URL(request.url).pathname).toStrictEqual('/auth/login');
+          return expect(request.json()).resolves.toStrictEqual(credential);
+        });
+      });
+
+      it('When token is defined, should return expected body data', () => {
+        fetch.once(JSON.stringify({ token: '...' }));
+
+        const credential = {
+          username: 'gcrypt@gcrypt.globo.com',
+          password: 'awesome-security-testing',
+          token: '312456',
+        };
+
+        return client.login(credential).then(() => {
+          expect(fetch).toHaveBeenCalled();
+          const request = fetch.mock.calls[0][0];
+          expect(request.method).toStrictEqual('POST');
+          expect(request.headers.get('Content-Type')).toContain('application/json');
+          expect(new URL(request.url).pathname).toStrictEqual('/auth/login');
+          return expect(request.json()).resolves.toStrictEqual(credential);
+        });
+      });
+
+      it('When login is unsuccessful, should reject with servers response', () => {
+        const errorMessage = 'Wrong credential';
+        fetch.once(errorMessage, { status: 401 });
+
+        const credential = {
+          username: 'gcrypt@gcrypt.globo.com',
+          password: 'awesome-security-testing',
+          token: '312456',
+        };
+
+        return expect(client.login(credential)).rejects.toThrowError(errorMessage);
+      });
+
+      it('When login is successful but the JSON has no token field, should reject', () => {
+        fetch.once('{}');
+
+        const credential = {
+          username: 'gcrypt@gcrypt.globo.com',
+          password: 'awesome-security-testing',
+          token: '312456',
+        };
+
+        return expect(client.login(credential))
+          .rejects.toThrowError('unexpected message received from remote server');
+      });
+
+      it('When login is successful, should store the token in web storage', () => {
+        const serverResponse = {
+          token: 'just another jwt',
+        };
+
+        fetch.once(JSON.stringify(serverResponse));
+
+        const credential = {
+          username: 'gcrypt@gcrypt.globo.com',
+          password: 'awesome-security-testing',
+          token: '312456',
+        };
+
+        return client.login(credential).then(token => expect(client.sessionStorage.retrieve())
+          .toStrictEqual(token));
+      });
+    });
+  });
+});
+
+describe('BrowserSessionStorage', () => {
+  const clearStorage = (storage) => {
+    Array.from(Array(storage.length).keys())
+      .forEach(index => storage.removeItem(storage.key(index)));
+  };
+
+  const clearAllStorages = () => {
+    clearStorage(window.localStorage);
+    clearStorage(window.sessionStorage);
+  };
+
+  beforeEach(clearAllStorages);
+
+  afterEach(clearAllStorages);
+
+  describe('constructor', () => {
+    it('When no one param is defined, should use default values', () => {
+      const sessionStorage = new BrowserSessionStorage();
+      expect(sessionStorage.key).toStrictEqual('_gcrypt_token');
+      expect(sessionStorage.storage).toBe(window.localStorage);
+    });
+
+    it('When using custom settings, should ensure expected field on instance', () => {
+      const key = 'another-session-key';
+      const storage = window.sessionStorage;
+      const sessionStorage = new BrowserSessionStorage(key, storage);
+      expect(sessionStorage.key).toStrictEqual(key);
+      expect(sessionStorage.storage).toStrictEqual(storage);
+    });
+  });
+
+  describe('clear', () => {
+    it('When cleaning the key in storage, ensure it was deleted', () => {
+      const token = 'awesome-gcrypt-token';
+      const storage = window.sessionStorage;
+      const sessionStorage = new BrowserSessionStorage(null, storage);
+      storage.setItem(sessionStorage.key, token);
+      expect(sessionStorage.storage.getItem(sessionStorage.key)).toEqual(token);
+      sessionStorage.clear();
+      expect(sessionStorage.storage.getItem(sessionStorage.key)).toEqual(null);
+    });
+  });
+
+  describe('retrieve', () => {
+    it('When storage is empty, should return null value', () => {
+      const storage = new BrowserSessionStorage();
+      expect(storage.retrieve()).toEqual(null);
+    });
+
+    it('When storage is not empty, should return expected token', () => {
+      const token = 'awesome-gcrypt-token';
+      const storage = new BrowserSessionStorage();
+      storage.storage.setItem(storage.key, token);
+      expect(storage.retrieve()).toStrictEqual(token);
+    });
+  });
+
+  describe('save', () => {
+    it('When saving a token, ensure it really was stored', () => {
+      const token = 'awesome-gcrypt-token';
+      const storage = new BrowserSessionStorage();
+      storage.save(token);
+      expect(storage.storage.getItem(storage.key)).toStrictEqual(token);
     });
   });
 });
--- core/src/browser.js
+++ core/src/browser.js
@@ -2,14 +2,60 @@
 /* eslint new-cap: ["error", { "newIsCap": false }] */
 
 /**
+ * Create a new instance of session's storage compatible with browsers.
+ *
+ * @constructor
+ * @param {string} [key=_gcrypt_token] - The used to hold session token.
+ * @param {Storage} [storage=window.localStorage] - The location where data is
+ * stored.
+ */
+function BrowserSessionStorage(key, storage) {
+  this.key = key != null ? key : '_gcrypt_token';
+  this.storage = storage != null ? storage : window.localStorage;
+}
+
+/**
+ * Deletes any token mapped.
+ *
+ * @method
+ */
+BrowserSessionStorage.prototype.clear = function () {
+  this.storage.removeItem(this.key);
+};
+
+/**
+ * Retrieves a session token from browser strorage. When there is no token,
+ * returns null value.
+ *
+ * @method
+ * @returns {string?} The session token.
+ */
+BrowserSessionStorage.prototype.retrieve = function () {
+  return this.storage.getItem(this.key);
+};
+
+/**
+ * Saves the session token into browser's storage, overwrites older token.
+ *
+ * @method
+ * @param {string!} token - The user's session token.
+ */
+BrowserSessionStorage.prototype.save = function (token) {
+  this.storage.setItem(this.key, token);
+};
+
+/**
  * @typedef BrowserClientSettings
  * @type {object}
- * @property {any} fetch - Implementation of Fetch API.
+ * @property {any?} fetch - Implementation of Fetch API.
+ * @property {BrowserSessionStorage?} sessionStorage - A session storage
+ * compatible with browser.
  */
 
 /**
  * Creates a client instance of GCrypt.
  *
+ * @constructor
  * @param {string!} target An URL formatted string.
  * @param {BrowserClientSettings?} settings Additional settings about client.
  */
@@ -26,14 +72,18 @@ function BrowserClient(target, settings) {
 
   const newSettings = settings != null ? settings : {};
 
-  this.fetch = newSettings.fetch != null ? newSettings.fetch : window.fetch;
+  this.fetch = newSettings.fetch != null
+    ? newSettings.fetch : window.fetch;
+
+  this.sessionStorage = newSettings.sessionStorage != null
+    ? newSettings.sessionStorage : new BrowserSessionStorage();
 }
 
 /**
  * Checks whether the remote server is working or not.
  *
  * @async
- * @function health
+ * @method
  * @returns {Promise<boolean>} A promise of a boolean value related to the
  * server status.
  */
@@ -42,4 +92,57 @@ BrowserClient.prototype.health = function () {
     .then(response => response.ok);
 };
 
-export default BrowserClient;
+/**
+ * @typedef Credential Represents a general crendetial from user.
+ * @type {NativeCredential}
+ */
+
+/**
+ * @typedef NativeCredential Represents a traditional credential from a user.
+ *
+ * @type {object}
+ * @property {string} username - The user's identifier (e.g. email).
+ * @property {string} password - The user's password.
+ * @property {string?} token - The user's Two factor token.
+ */
+
+/**
+ * Attempts sign-in on remote server. If successful, returns a valid JWT string
+ * for usage against authenticated endpoint on server.
+ *
+ * @async
+ * @method
+ * @param {Credential!} credential - The user's credential.
+ * @return {Promise<string>} A valid for future use JavaScript Web Token.
+ */
+BrowserClient.prototype.login = function (credential) {
+  if (credential == null) throw new Error('credential is required');
+
+  const payload = {
+    username: credential.username,
+    password: credential.password,
+  };
+
+  if (credential.token != null) payload.token = credential.token;
+
+  const request = new Request(`${this.target}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  return this.fetch(request).then(response => (response.ok
+    ? response.json()
+    : response.text().then(message => Promise.reject(new Error(message)))))
+    .then(json => (json.token != null
+      ? json.token
+      : Promise.reject(new Error('unexpected message received from remote server'))))
+    .then((token) => {
+      this.sessionStorage.save(token);
+      return token;
+    });
+};
+
+export { BrowserClient, BrowserSessionStorage };
