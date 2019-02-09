614a0e505310abbbfae96dce35c4cf7b91a9480e
Claudio Netto <nettinhorama@gmail.com>
2019-02-09 21:01:02 -0200

    app: start GCrypt API

--- api/src/index.js
+++ api/src/index.js
@@ -4,4 +4,31 @@
  * License: MIT
  */
 
-// TODO: implement the entrypoint.
+import dotenv from 'dotenv';
+
+import Routes from './web/routes';
+import App from './cmd/app';
+import WebServer from './web/webserver';
+
+function main(cmd, options) {
+  const opts = typeof cmd === 'string' ? options : cmd;
+
+  const [address, port] = opts.webserverAddress.split(':');
+  const certificate = opts.tlsCertificate;
+  const key = opts.tlsKey;
+
+  const webserver = new WebServer(Routes, { address, port, certificate, key });
+
+  const shutdownWebServer = () => webserver.close();
+  
+  process.on('SIGINT', shutdownWebServer);
+  process.on('SIGTERM', shutdownWebServer);
+
+  webserver.listen();
+}
+
+dotenv.config();
+
+new App()
+  .action(main)
+  .parse(process.argv);
